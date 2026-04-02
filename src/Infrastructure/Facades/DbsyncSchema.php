abfd69b6bf723fc99122ea69e59ef0c5e53ed545
Adrian Canals <a.canals14@gmail.com>
2026-04-02 12:28:27 +0200

    (fix) Arreglar Fachada DbsyncSchema.

--- src/Infrastructure/Facades/DbsyncSchema.php
+++ src/Infrastructure/Facades/DbsyncSchema.php
@@ -8,10 +8,12 @@ use Illuminate\Support\Facades\Facade;
 use Thehouseofel\Dbsync\Domain\Contracts\SchemaFactory;
 
 /**
- * @method static \Thehouseofel\Dbsync\Domain\Support\SchemaManager connection(\Illuminate\Database\Connection|string|null $connection)
+ * @method static \Thehouseofel\Dbsync\Domain\Support\SchemaConnection connection(\Illuminate\Database\Connection|string|null $connection)
+ * @method static bool isOracle()
  * @method static void forceDrop(string $table)
  * @method static void truncate(array $tables)
  * @method static void syncIdentity(string $table, string $column = 'id')
+ * @method static void disableBuffer()
  *
  * @see \Thehouseofel\Dbsync\Domain\Support\SchemaManager
  */

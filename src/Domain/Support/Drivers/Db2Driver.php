71a9e6fc6b52427edd7246facbc17d4d1fd7e36f
Adrian Canals <a.canals14@gmail.com>
2026-09-23 18:12:57 +0200

    db2: Añadir validación paquete "easi-power/laravel-db2"

--- src/Domain/Support/Drivers/Db2Driver.php
+++ src/Domain/Support/Drivers/Db2Driver.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Dbsync\Domain\Support\Drivers;
 
+use Composer\InstalledVersions;
+
 class Db2Driver extends BaseDriver
 {
+    protected function validateVersion(): void
+    {
+        $package = 'easi-power/laravel-db2';
+
+        if (! InstalledVersions::isInstalled($package)) {
+            throw new \RuntimeException("The package '$package' is not installed. Please install it to use the DB2 driver.");
+        }
+
+        $version = InstalledVersions::getVersion($package);
+
+        if ($version === null || version_compare($version, '13.0', '<')) {
+            throw new \RuntimeException("The installed version of '$package' is not supported. Please install version ^13.0.");
+        }
+    }
+
     public function forceDrop(string $table): void
     {
         // DB2 drops the foreign keys that reference the table along with the table.
